66f0dd41da354d1200a7d522f722595ee6b05641
grandsisters <grandsisters@naver.com>
2021-10-22 13:21:12 +0900

    이미지를 눌렀을때도 작동하게끔 추가

--- 20211020/accordion2.js
+++ 20211020/accordion2.js
@@ -1,23 +1,26 @@
+// $(".items").click((e) => {
+//   const haveIt = $(e.target).hasClass("name-tag");
+//   const isRun = $(e.currentTarget).hasClass("more");
+//   if (haveIt) {
+//     $(".items").removeClass("more");
+//     if (!isRun) {
+//       $(e.currentTarget).addClass("more");
+//     }
+//   }
+// });
+
 $(".items").click((e) => {
-  const haveIt = $(e.target).hasClass("name-tag");
-  const isRun = $(e.currentTarget).hasClass("more");
-  if (haveIt) {
+  const isHead = $(e.target).hasClass("name-tag");
+  const isImg = $(e.target).hasClass("tag-img");
+  if (!isHead && !isImg) return;
+
+  if (isHead || isImg) {
+    const isActive = $(e.currentTarget).hasClass("more");
     $(".items").removeClass("more");
-    if (!isRun) {
+    if (!isActive) {
       $(e.currentTarget).addClass("more");
+      $(".contents").slideUp();
+      $(".items.more .contents").slideDown();
     }
   }
 });
-
-// $(".items").click((e) => {
-//   const isHead = $(e.target).hasClass("name-tag");
-//   if (!isHead) return;
-
-//   const isActive = $(e.currentTarget).hasClass("more");
-//   $(".items").removeClass("more");
-//   if (!isActive) {
-//     $(e.currentTarget).addClass("more");
-//     $(".contents").slideUp();
-//     $(".items.more .contents").slideDown();
-//   }
-// });
