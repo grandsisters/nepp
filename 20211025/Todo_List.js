5fcec8a2be72dbf42d20140f3625a76c208201d8
grandsisters <grandsisters@naver.com>
2021-10-26 12:41:19 +0900

    전체 삭제, 수정 기능 추가 및 localstorage 맛보기

--- 20211025/Todo_List.js
+++ 20211025/Todo_List.js
@@ -1,3 +1,20 @@
+const init = () => {
+  const value = localStorage.todoList;
+  const todo = `
+  <li class="each-list">
+  <label class="check-text" for="delete-btn">
+  <input type="checkbox" class="input-check" />
+  <span class="list-text">${value}</span> </label>
+  <button class="edit-btn">수정</button
+  ><button class="delete-btn">삭제</button>
+  </li>
+  `;
+
+  $(".lists").append(todo);
+};
+
+init();
+
 $(".form-add").on("submit", (e) => {
   e.preventDefault();
   const value = $("#input-todo").val();
@@ -5,20 +22,59 @@ $(".form-add").on("submit", (e) => {
 
   const todo = `
   <li class="each-list">
-  <label class="check-text" for="delete-btn">
-    <input type="checkbox" name="" id="" />
-    <span class="list-text"
-      >${value}</span
-    > </label
+  <label class="check-text" for="delete-btn"> 
+  <input type="checkbox" class="input-check" /> 
+  <span class="list-text">${value}</span> </label>
+  <button class="edit-btn">수정</button
   ><button class="delete-btn">삭제</button>
-</li>`;
+  </li>
+  `;
 
   $(".lists").append(todo);
   $("#input-todo").val("");
   $("#input-todo").focus();
+
+  // 실행이 안됨
+  // const rawTodoList = localStorage.todoList || "[]";
+  // const todoList = JSON.parse(rawTodoList);
+  // todoList.push(value);
+  // localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  // console.log(localStorage.todoList);
+  // localStorage.setItem("todo", value);
 });
 
-$(".delete-btn").on("click", (e) => {
+// 이렇게 하면 새로추가 된 li요소들은 삭제가 안된다.
+// $(".delete-btn").on("click", (e) => {
+//   const x = e.currentTarget.closest("li");
+//   x.remove();
+// });
+
+$(document).on("click", ".delete-btn", (e) => {
   const x = e.currentTarget.closest("li");
   x.remove();
+});
+
+$(document).on("change", ".input-check", (e) => {
+  const isChecked = e.currentTarget.checked;
+  const nearItem = e.currentTarget.closest("li");
+  if (isChecked) {
+    $(nearItem).addClass("active");
+  } else {
+    $(nearItem).removeClass("active");
+  }
+});
+
+$(".deleteAll-btn").click(() => {
+  $("ul").empty();
+});
+
+$(document).on("click", ".edit-btn", (e) => {
+  const $todoContent = $(e.currentTarget).parent().find(".list-text");
+  const preValue = $todoContent.text();
+  const content = prompt("수정할 내용을 입력하세요", preValue);
+
+  // 수정할 내용이 있는 경우에만 변경하기
+  // 사실 기본값이 있기 때문에 그렇게 쓸모는 없다고 봄
+  if (content) $todoContent.text(content);
 });
