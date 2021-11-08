5150cf2d03a1021b7d399a7769076c227a1b7562
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-11-09 00:22:00 +0900

    todo 추가 (node.js)

--- news-api/app.js
+++ news-api/app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var todoRouter = require("./routes/todo");
 var usersRouter = require("./routes/users");
 
 var app = express();
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/todo", todoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ news-api/routes/todo.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  const todos = [
+    { isDone: true, content: "장보기" },
+    { isDone: false, content: "공부하기" },
+  ];
+
+  res.render("todo", { todos });
+});
+
+router.post("/", (req, res) => {
+  // TODO 생성
+});
+
+router.get("/:id", (req, res) => {
+  // TODO 생성 (특정 id를 받아와서 수정 하고 싶을때)
+});
+
+router.put("/:id", (req, res) => {
+  // TODO 수정
+});
+
+router.patch("/:id", (req, res) => {
+  // TODO 일부 수정
+});
+
+router.delete("/:id", (req, res) => {
+  // TODO 삭제
+});
+module.exports = router;
