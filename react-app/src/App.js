d64b2f18142e9ad6c783b9cf5a8dc9617e2d453b
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-11-22 13:05:32 +0900

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,17 @@
 // import Counter from "./components/Counter";
 // import EventPractice from "./components/EventPractice";
-import IterationSample from "./components/IterationSample";
+// import Average from "./components/Average";
+// import IterationSample from "./components/IterationSample";
 // import Test2 from "./components/Test2";
 // import BendingMachine from "./components/BendingMachine";
 // import Sort from "./components/Sort";
 // import Counters2 from "./components/Counters2";
 // import Info from "./components/Info";
 // import { useState } from "react";
+
+import TodoComponent from "./components/Todo";
+
+// import StyledComponents from "./components/StyledComponents";
 
 function App() {
   // const [visible, setVisible] = useState(false);
@@ -17,12 +22,15 @@ function App() {
       {/* <Say2 /> */}
       {/* <EventPractice /> */}
       {/* <Test2 /> */}
-      <IterationSample />
+      {/* <IterationSample /> */}
       {/* <BendingMachine /> */}
       {/* <Sort /> */}
       {/* <Counters2 /> */}
       {/* <button onClick={() => setVisible(!visible)}>{visible ? "숨기기" : "보이기"}</button>
       {visible && <Info />} */}
+      {/* <Average /> */}
+      {/* <StyledComponents /> */}
+      <TodoComponent />
     </>
   );
 }
