7d88dbf0d0601afcaaab3147739b2080a8c8c1a7
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-11-23 12:37:31 +0900

    Update Todo.jsx

--- react-app/src/components/Todo.jsx
+++ react-app/src/components/Todo.jsx
@@ -1,30 +1,53 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { useState } from "react";
 
 const TodoComponent = () => {
+  const [content, setContent] = useState("");
+  const [todoList, setTodoList] = useState([
+    { id: 1, text: "할일", isDone: false },
+    { id: 2, text: "할일", isDone: false },
+  ]);
+  const [nextId, setNextId] = useState(3);
+
+  const changedValue = (e) => {
+    console.log(e.target.value);
+    setContent(e.target.value);
+  };
+  const addList = () => {
+    setNextId(nextId + 1);
+    setTodoList([...todoList, { id: nextId, text: content, isDone: false }]);
+    console.log(todoList);
+  };
+  const deleteList = (id) => {
+    setTodoList(todoList.filter((e) => e.id !== id));
+  };
+  const handleToggle = (e, id) => {
+    const isDone = e.target.checked;
+    const newArray = todoList.map((todo) => {
+      return todo.id === id ? { ...todo, isDone: isDone } : todo;
+    });
+    setTodoList(newArray);
+  };
+
   return (
     <>
       <Container>
         <Title>일정 관리</Title>
         <InputWrapper>
-          <Input />
-          <BtnAdd>등록</BtnAdd>
+          <Input onChange={changedValue} placeholder="할 일을 입력하세요." />
+          <BtnAdd onClick={addList}>등록</BtnAdd>
         </InputWrapper>
         <TodoList>
-          <TodoItem>
-            <label>
-              <input type="checkbox" />
-              <TodoContent>할일</TodoContent>
-            </label>
-            <BtnDelete>삭제</BtnDelete>
-          </TodoItem>
-          <TodoItem>
-            <label>
-              <input type="checkbox" />
-              <TodoContent>할일</TodoContent>
-            </label>
-            <BtnDelete>삭제</BtnDelete>
-          </TodoItem>
+          {todoList.map(({ id, text, isDone }) => (
+            <TodoItem key={id} isDone={isDone}>
+              <label>
+                <input type="checkbox" onChange={(e) => handleToggle(e, id)} />
+                <TodoContent>{text}</TodoContent>
+              </label>
+              <BtnDelete onClick={() => deleteList(id)}>삭제</BtnDelete>
+            </TodoItem>
+          ))}
         </TodoList>
       </Container>
     </>
@@ -66,17 +89,18 @@ const BtnAdd = styled.button`
   position: absolute;
   right: 5px;
   top: 3px;
-  /* border: black; */
 
   &:hover {
     cursor: pointer;
   }
 `;
-
 const TodoList = styled.ul`
   list-style: none;
   margin: 0;
   padding-left: 0;
+`;
+const TodoContent = styled.span`
+  font-weight: bold;
 `;
 const TodoItem = styled.li`
   border-bottom: 1px solid black;
@@ -85,9 +109,20 @@ const TodoItem = styled.li`
   position: relative;
   display: flex;
   align-items: center;
-`;
-const TodoContent = styled.span`
-  font-weight: bold;
+
+  ${TodoContent} {
+    text-decoration: ${(props) => props.isDone && "line-through"};
+  }
+
+  ${({ isDone }) =>
+    isDone &&
+    css`
+      background: black;
+      color: white;
+      ${TodoContent} {
+        text-decoration: line-through;
+      }
+    `}
 `;
 const BtnDelete = styled.button`
   position: absolute;
