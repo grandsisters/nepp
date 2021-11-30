3143050b626bc282e325ec175e881d00e1446422
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-11-30 11:17:52 +0900

    Update Accordian2.jsx

--- react-app/src/components/bootstrap/Accordian2.jsx
+++ react-app/src/components/bootstrap/Accordian2.jsx
@@ -2,25 +2,42 @@ import React, { useState } from "react";
 import styled, { css } from "styled-components";
 
 const Accordian2 = () => {
-  const [Lists, setLists] = useState([
-    { id: 1, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
-    { id: 2, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
-    { id: 3, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
-    { id: 4, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
-  ]);
+  // const [Lists, setLists] = useState([
+  //   { id: 1, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
+  //   { id: 2, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
+  //   { id: 3, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
+  //   { id: 4, title: "This is Accordian", content: "Hi Hello 안녕", active: false },
+  // ]);
+
+  const Lists = [
+    { id: 0, title: "This is Accordian", content: "Hi Hello 안녕" },
+    { id: 1, title: "This is Accordian", content: "Hi Hello 안녕" },
+    { id: 2, title: "This is Accordian", content: "Hi Hello 안녕" },
+    { id: 3, title: "This is Accordian", content: "Hi Hello 안녕" },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // const handleClick = (id) => {
+  //   const newList = Lists.map((x) => ({
+  //     ...x,
+  //     active: x.id === id ? !x.active : false,
+  //   }));
+  //   setLists(newList);
+  // };
 
   const handleClick = (id) => {
-    const newList = Lists.map((x) => ({
-      ...x,
-      active: x.id === id ? !x.active : false,
-    }));
-    setLists(newList);
+    if (activeIndex === id) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(id);
+    }
   };
 
   return (
     <ListBox>
-      {Lists.map((e) => (
-        <List active={e.active}>
+      {Lists.map((e, i) => (
+        <List active={activeIndex === i}>
           <Header onClick={() => handleClick(e.id)}>{e.title}</Header>
           <Body>{e.content}</Body>
         </List>
