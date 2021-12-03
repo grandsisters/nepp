39ed1f1e47a7ea5d9f86739720b0a944513954d2
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-03 13:25:00 +0900

    Create Modal.jsx

---
+++ react-app/src/components/bootstrap/Modal.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+const Modal = () => {
+  const [activeModal, setActiveModal] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  const handleNo = () => {
+    setFadeOut(true);
+
+    setTimeout(() => {
+      setActiveModal(false);
+      setFadeOut(false);
+    }, 300);
+  };
+  const anotherWorld = () => {
+    setActiveModal(true);
+  };
+
+  return (
+    <>
+      <ModalFire onClick={anotherWorld}>모달 커몬</ModalFire>
+      {activeModal && (
+        <BackDrop onClick={handleNo}>
+          <Container fadeOut={fadeOut}>
+            <HeadLine>
+              <Title>이세계로 전생</Title>
+              <XBtn onClick={handleNo}>X</XBtn>
+            </HeadLine>
+            <Content>계속하시겠습니까?</Content>
+            <YesBtn>
+              <a href="https://www.naver.com" target="_blank" rel="noreferrer">
+                예
+              </a>
+            </YesBtn>
+            <NoBtn onClick={handleNo}>아니오</NoBtn>
+          </Container>
+        </BackDrop>
+      )}
+    </>
+  );
+};
+
+const ModalFire = styled.button``;
+
+const BackDropOp = keyframes`
+  from {
+    opacity: 0
+  }
+  to {
+    opacity: 0.8
+  }
+`;
+
+const BackDrop = styled.div`
+  animation: ${BackDropOp} 0.3s ease-in;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  opacity: 0.8;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
+
+const ModalSlide = keyframes`
+  from {
+    transform: translate(-50%, -30px)
+  }
+  to{
+    transform: translate(-50%, 0)
+  }
+`;
+
+const Container = styled.div`
+  animation: ${ModalSlide} 0.3s ease-in;
+  width: 350px;
+  height: 150px;
+  background: white;
+  border-radius: 4px;
+  border: 1px solid white;
+  position: fixed;
+  z-index: 100;
+  top: 5%;
+  left: 50%;
+  transform: ${({ fadeOut }) => (fadeOut ? "translate(-50%, -30px)" : "translateX(-50%)")};
+  transition: transform 0.3s ease-out, opacity 0.3s ease-out;
+`;
+
+const HeadLine = styled.div`
+  position: relative;
+  padding: 10px;
+  border-bottom: 1px solid black;
+`;
+
+const Title = styled.span`
+  font-weight: bolder;
+`;
+
+const XBtn = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  cursor: pointer;
+`;
+
+const Content = styled.div`
+  padding: 15px;
+  border-bottom: 1px solid black;
+`;
+
+const Btn = styled.button`
+  position: absolute;
+  bottom: 10px;
+  width: 60px;
+  height: 35px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const YesBtn = styled(Btn)`
+  right: 80px;
+  background: blue;
+
+  & > a {
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const NoBtn = styled(Btn)`
+  right: 10px;
+  background: grey;
+`;
+
+export default Modal;
