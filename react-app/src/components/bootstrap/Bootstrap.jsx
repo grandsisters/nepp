301797ec9617f70d166a40558558fcd3c4fff0a3
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-06 11:31:56 +0900

    Update Bootstrap.jsx

--- react-app/src/components/bootstrap/Bootstrap.jsx
+++ react-app/src/components/bootstrap/Bootstrap.jsx
@@ -12,13 +12,12 @@ import Modal from "./Modal";
 //   { id: 3, title: "Title3", content: "asdsdsadawdasdawdasdawd", active: false },
 //   { id: 4, title: "Title4", content: "asdsdsadawdasdawdasdawd", active: false },
 // ];
-
-const imageList = [
-  "https://t1.daumcdn.net/cfile/tistory/9946A4505F5817A60D",
-  "https://cdn.pixabay.com/photo/2019/08/01/12/36/illustration-4377408_960_720.png",
-  "https://mblogthumb-phinf.pstatic.net/MjAxNzAxMDdfMTE2/MDAxNDgzNzU1MDMyNTc0.hx5KAnplO4tzx7gyHo0OXzPvyCH5h7gfLjMf-S5qamAg.oUP8V58A0P0ZybvJw_-Dq57FbxOxV9o1MU4tRdnbORwg.JPEG.candle_yt/4.jpg?type=w800",
-  "https://t1.daumcdn.net/blogfile/fs11/23_blog_2008_07_13_10_37_48795c5e1d9b4?x-content-disposition=inline&filename=haeyongj_20.jpg",
-];
+// const imageList = [
+//   "https://t1.daumcdn.net/cfile/tistory/9946A4505F5817A60D",
+//   "https://cdn.pixabay.com/photo/2019/08/01/12/36/illustration-4377408_960_720.png",
+//   "https://mblogthumb-phinf.pstatic.net/MjAxNzAxMDdfMTE2/MDAxNDgzNzU1MDMyNTc0.hx5KAnplO4tzx7gyHo0OXzPvyCH5h7gfLjMf-S5qamAg.oUP8V58A0P0ZybvJw_-Dq57FbxOxV9o1MU4tRdnbORwg.JPEG.candle_yt/4.jpg?type=w800",
+//   "https://t1.daumcdn.net/blogfile/fs11/23_blog_2008_07_13_10_37_48795c5e1d9b4?x-content-disposition=inline&filename=haeyongj_20.jpg",
+// ];
 
 const Bootstrap = () => {
   // const [data, setData] = useState(accordianData);
