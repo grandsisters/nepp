d291a50b5bbf4229c2fd87da91905b2c354b6cd3
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-07 13:46:22 +0900

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,47 +1,9 @@
-// import Counter from "./components/Counter";
-// import EventPractice from "./components/EventPractice";
-// import Average from "./components/Average";
-// import IterationSample from "./components/IterationSample";
-// import Test2 from "./components/Test2";
-// import BendingMachine from "./components/BendingMachine";
-// import Sort from "./components/Sort";
-// import Counters2 from "./components/Counters2";
-// import Info from "./components/Info";
-// import { useState } from "react";
-// import Axios from "./components/Axios";
-// import TodoComponent from "./components/Todo";
-// import StyledComponents from "./components/StyledComponents";
-// import News from "./components/news/News";
-
-// import Carousel from "./components/bootstrap/Carousel";
-
-// import TodoComponent2 from "./components/todo/Todo2";
-
-import Bootstrap from "./components/bootstrap/Bootstrap";
+import Youtube from "./Youtube/Youtube";
 
 function App() {
-  // const [visible, setVisible] = useState(false);
   return (
     <>
-      {/* <Counter /> */}
-      {/* <Counter name="first">버튼만들기</Counter> */}
-      {/* <Say2 /> */}
-      {/* <EventPractice /> */}
-      {/* <Test2 /> */}
-      {/* <IterationSample /> */}
-      {/* <BendingMachine /> */}
-      {/* <Sort /> */}
-      {/* <Counters2 /> */}
-      {/* <button onClick={() => setVisible(!visible)}>{visible ? "숨기기" : "보이기"}</button>
-      {visible && <Info />} */}
-      {/* <Average /> */}
-      {/* <StyledComponents /> */}
-      {/* <TodoComponent /> */}
-      {/* <Axios /> */}
-      {/* <News /> */}
-      <Bootstrap />
-      {/* <TodoComponent2 /> */}
-      {/* <Carousel /> */}
+      <Youtube />
     </>
   );
 }
