cc0e0f558a9178274648d2682bd2ec6dbbf39dec
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-13 13:43:59 +0900

    Update Router.js

--- naver/src/Router.js
+++ naver/src/Router.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./components/pages/Main";
-import Movie from "./components/pages/Movie";
+import { Main, Movie, Book } from "./components/pages";
+import { TopNav } from "./components/organisms";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/movie" element={<Movie />} />
+        <Route path="/Movie" element={<Movie />} />
+        <Route path="/Book" element={<Book />} />
       </Routes>
     </BrowserRouter>
   );
