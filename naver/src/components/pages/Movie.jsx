95b673cb0b48438d3568ae3a6d9e0e95828293ec
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-13 13:44:31 +0900

    pages

--- naver/src/components/pages/Movie.jsx
+++ naver/src/components/pages/Movie.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { MovieList } from "../organisms";
+import { useState } from "react";
 
 const Movie = () => {
-  return <div>영화</div>;
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(inputValue);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <InputQuery onChange={(e) => setInputValue(e.target.value)} placeholder="검색어를 입력하세요" />
+        <button>검색</button>
+      </Form>
+      <MovieList />
+    </div>
+  );
 };
+
+const Form = styled.form`
+  display: flex;
+`;
+
+const InputQuery = styled.input`
+  flex: 1;
+  margin-right: 10px;
+`;
 
 export default Movie;
