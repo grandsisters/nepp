fe6d638034a3ca975aa8f33c3890554450b334bd
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-14 12:46:41 +0900

    pages

--- naver/src/components/pages/Book.jsx
+++ naver/src/components/pages/Book.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import { BookList } from "../organisms/";
+import { useState } from "react";
+import { getBookList } from "../../apis/book";
 
 const Book = () => {
+  const [query, setQuery] = useState("");
+  const [bookList, setBookList] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const params = { query: query };
+    const result = await getBookList(params);
+    setBookList(result.items);
+  };
+
   return (
-    <>
-      <div>책</div>
-    </>
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <InputQuery onChange={(e) => setQuery(e.target.value)} placeholder="검색어를 입력하세요" />
+        <button>검색</button>
+      </Form>
+      <BookList data={bookList} />
+    </div>
   );
 };
+
+const Form = styled.form`
+  display: flex;
+`;
+
+const InputQuery = styled.input`
+  flex: 1;
+  margin-right: 10px;
+`;
 
 export default Book;
--- naver/src/components/pages/Movie.jsx
+++ naver/src/components/pages/Movie.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import { MovieList } from "../organisms";
 import { useState } from "react";
+import { getMovieList } from "../../apis/movie";
 
 const Movie = () => {
-  const [inputValue, setInputValue] = useState("");
-  const handleSubmit = (e) => {
+  const [query, setQuery] = useState("");
+  const [movieList, setMovieList] = useState([]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(inputValue);
+    const params = { query: query };
+    const result = await getMovieList(params);
+    setMovieList(result.items);
   };
 
   return (
     <div>
       <Form onSubmit={handleSubmit}>
-        <InputQuery onChange={(e) => setInputValue(e.target.value)} placeholder="검색어를 입력하세요" />
+        <InputQuery onChange={(e) => setQuery(e.target.value)} placeholder="검색어를 입력하세요" />
         <button>검색</button>
       </Form>
-      <MovieList />
+      <MovieList data={movieList} />
     </div>
   );
 };
