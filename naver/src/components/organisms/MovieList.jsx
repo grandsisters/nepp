fd04fe654613a03c675c40678f0581becce7eff7
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-14 12:46:20 +0900

    organisms

--- naver/src/components/organisms/MovieList.jsx
+++ naver/src/components/organisms/MovieList.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const MovieList = () => {
+const MovieList = ({ data }) => {
+  console.log(data);
   return (
     <List>
-      <Item>
-        <Image src="https://image.newsis.com/2019/05/21/NISI20190521_0000330654_web.jpg?rnd=20190521141743" />
-        <Title>기생충</Title>
-      </Item>
+      {data.map((e) => (
+        <a href={e.link} target={"_blank"} rel="noreferrer" key={e.link}>
+          <Item>
+            <Image src={e.image} />
+            {/* <Title>{e.title}</Title> */}
+            {/* b태그를 없앨수있지만 해킹 위험이 있음 */}
+            <Title dangerouslySetInnerHTML={{ __html: e.title }} />
+            <p>개봉년도:{e.pubDate}</p>
+            <p>유저평점:{e.userRating}</p>
+          </Item>
+        </a>
+      ))}
     </List>
   );
 };
--- naver/src/components/organisms/index.js
+++ naver/src/components/organisms/index.js
@@ -1,2 +1,3 @@
 export { default as TopNav } from "./TopNav";
 export { default as MovieList } from "./MovieList";
+export { default as BookList } from "./BookList";
