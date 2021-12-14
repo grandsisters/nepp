1eabfd2cca32e6636cc5b676058114841faf9f60
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-14 12:45:59 +0900

    Create book.js

---
+++ naver/src/apis/book.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const getBookList = async (params) => {
+  const result = await axios.get("v1/search/book.json", {
+    headers: { "X-Naver-Client-Id": "6YAG7XSslp4VZ4yRmaIA", "X-Naver-Client-Secret": "6WIqDiVobU" },
+    params,
+  });
+
+  return result.data;
+};
