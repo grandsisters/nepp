eff9c0a6f1fca5704e1dbf20d4f0b6a49f1dd6a0
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-14 12:46:02 +0900

    Update movie.js

--- naver/src/apis/movie.js
+++ naver/src/apis/movie.js
@@ -1,3 +1,10 @@
-export const getMovieList = (params) => {
-  alert(params);
+import axios from "axios";
+
+export const getMovieList = async (params) => {
+  const result = await axios.get("v1/search/movie.json", {
+    headers: { "X-Naver-Client-Id": "6YAG7XSslp4VZ4yRmaIA", "X-Naver-Client-Secret": "6WIqDiVobU" },
+    params,
+  });
+
+  return result.data;
 };
