04dce401c6fac1536ad51cf1bdf85ff11ded79e0
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-17 13:16:11 +0900

    Update book.js

--- naver/src/apis/book.js
+++ naver/src/apis/book.js
@@ -8,3 +8,12 @@ export const getBookList = async (params) => {
 
   return result.data;
 };
+
+export const getBookInfo = async (params) => {
+  const result = await axios.get("/v1/search/book_adv.json", {
+    headers: { "X-Naver-Client-Id": "6YAG7XSslp4VZ4yRmaIA", "X-Naver-Client-Secret": "6WIqDiVobU" },
+    params,
+  });
+
+  return result.data;
+};
