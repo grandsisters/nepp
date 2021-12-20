ec0f358ff7983b9c9d9f50a9325c62c84d6f7e3d
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-20 17:51:02 +0900

    Update movie.js

--- naver/src/apis/movie.js
+++ naver/src/apis/movie.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const getMovieList = async (params) => {
-  const result = await axios.get("v1/search/movie.json", {
-    headers: { "X-Naver-Client-Id": "6YAG7XSslp4VZ4yRmaIA", "X-Naver-Client-Secret": "6WIqDiVobU" },
+  const result = await axios.get("https://localhost:8080/movie", {
     params,
   });
 
