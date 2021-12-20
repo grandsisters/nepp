0778f4c33d05c4032a09d64229d41ddc1d90cda5
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-20 16:54:22 +0900

    server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-module.exports = router;
+router.get("/movie", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/book", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/book/:isbn", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+export default router;
