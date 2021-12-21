38056e4a703e130465941dfa2696248e9814f3f5
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-21 13:45:28 +0900

    organisms

--- insta/src/components/organisms/index.js
+++ insta/src/components/organisms/index.js
@@ -3,3 +3,4 @@ export { default as Login_SB } from "./Login_SB";
 export { default as Signup_MB } from "./Signup_MB";
 export { default as Signup_SB } from "./Signup_SB";
 export { default as AppDownload } from "./AppDownload";
+export { default as Carousel } from "./Carousel";
