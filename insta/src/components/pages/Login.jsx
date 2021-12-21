84c1814f58891a73e2190121bb03fa59f4d89dc7
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-21 13:45:34 +0900

    pages

--- insta/src/components/pages/Login.jsx
+++ insta/src/components/pages/Login.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import styled from "styled-components";
-import { Login_MB, Login_SB, AppDownload } from "../organisms";
+import { Login_MB, Login_SB, AppDownload, Carousel } from "../organisms";
 
 const Login = () => {
   return (
     <Wrapper>
-      <Login_MB />
-      <Login_SB />
-      <AppDownload />
+      <Carousel />
+      <LoginBlock>
+        <Login_MB />
+        <Login_SB />
+        <AppDownload />
+      </LoginBlock>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  position: fixed;
+  top: 10%;
+  left: 25%;
+  display: flex;
+  align-items: center;
+`;
+
+const LoginBlock = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: fixed;
-  top: 10%;
-  left: 35%;
 `;
 
 export default Login;
