cc0d8f10a1cadc78da56715399921a04b7ac3984
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-23 14:07:19 +0900

    src

--- insta/src/Router.js
+++ insta/src/Router.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Signup } from "./components/pages";
+import { Login, Signup, Main } from "./components/pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
