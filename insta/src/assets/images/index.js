bffebfadcb774b22f5446fb185d31a9a2939d019
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-23 14:05:14 +0900

    nav_icon_images

---
+++ insta/src/assets/images/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as ImgHome } from "../../assets/images/home.svg";
+import { ReactComponent as Direct } from "../../assets/images/direct.svg";
+import { ReactComponent as New } from "../../assets/images/new.svg";
+import { ReactComponent as Find } from "../../assets/images/find.svg";
+import { ReactComponent as Activity } from "../../assets/images/activity.svg";
+
+export { ImgHome, Direct, New, Find, Activity };
