cddcdbe44dcdd53cd6a400560a9971521dcbc976
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-24 13:15:05 +0900

    images

--- insta/src/assets/images/index.js
+++ insta/src/assets/images/index.js
@@ -1,7 +1,11 @@
-import { ReactComponent as ImgHome } from "../../assets/images/home.svg";
-import { ReactComponent as Direct } from "../../assets/images/direct.svg";
-import { ReactComponent as New } from "../../assets/images/new.svg";
-import { ReactComponent as Find } from "../../assets/images/find.svg";
-import { ReactComponent as Activity } from "../../assets/images/activity.svg";
+import { ReactComponent as ImgHome } from "./home.svg";
+import { ReactComponent as Message } from "./message.svg";
+import { ReactComponent as New } from "./new.svg";
+import { ReactComponent as Find } from "./find.svg";
+import { ReactComponent as Activity } from "./activity.svg";
+import { ReactComponent as Comment } from "./comment.svg";
+import { ReactComponent as Bookmark } from "./bookmark.svg";
+import { ReactComponent as Emoticon } from "./emoticon.svg";
+import { ReactComponent as ETC } from "./etc.svg";
 
-export { ImgHome, Direct, New, Find, Activity };
+export { ImgHome, Message, New, Find, Activity, Comment, Bookmark, Emoticon, ETC };
