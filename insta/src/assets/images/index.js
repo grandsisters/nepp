41b2bfdade78af901b78a35673d1947c51fc265d
grandsisters <59759141+grandsisters@users.noreply.github.com>
2021-12-29 13:19:17 +0900

    images

--- insta/src/assets/images/index.js
+++ insta/src/assets/images/index.js
@@ -1,11 +1,15 @@
 import { ReactComponent as ImgHome } from "./home.svg";
-import { ReactComponent as Message } from "./message.svg";
-import { ReactComponent as New } from "./new.svg";
-import { ReactComponent as Find } from "./find.svg";
-import { ReactComponent as Activity } from "./activity.svg";
-import { ReactComponent as Comment } from "./comment.svg";
-import { ReactComponent as Bookmark } from "./bookmark.svg";
-import { ReactComponent as Emoticon } from "./emoticon.svg";
-import { ReactComponent as ETC } from "./etc.svg";
+import { ReactComponent as ImgMessage } from "./message.svg";
+import { ReactComponent as ImgNew } from "./new.svg";
+import { ReactComponent as ImgFind } from "./find.svg";
+import { ReactComponent as ImgActivity } from "./activity.svg";
+import { ReactComponent as ImgComment } from "./comment.svg";
+import { ReactComponent as ImgBookmark } from "./bookmark.svg";
+import { ReactComponent as ImgEmoticon } from "./emoticon.svg";
+import { ReactComponent as ImgETC } from "./etc.svg";
+import { ReactComponent as ImgProfile } from "./profile.svg";
+import { ReactComponent as ImgSaved } from "./saved.svg";
+import { ReactComponent as ImgSetting } from "./setting.svg";
+import { ReactComponent as ImgChangeAccount } from "./changeAccount.svg";
 
-export { ImgHome, Message, New, Find, Activity, Comment, Bookmark, Emoticon, ETC };
+export { ImgHome, ImgMessage, ImgNew, ImgFind, ImgActivity, ImgComment, ImgBookmark, ImgEmoticon, ImgETC, ImgProfile, ImgSaved, ImgSetting, ImgChangeAccount };
