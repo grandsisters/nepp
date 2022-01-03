f0682aaec0be2f4a70c32db2978d3305328dc10b
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-03 14:02:44 +0900

    Create MainRightData.js

---
+++ insta/src/data/MainRightData.js
@@ -0,0 +1,36 @@
+export const OthersData = [
+  {
+    image: "https://i.pinimg.com/736x/20/87/dc/2087dc617106100ab0716039317d3db8.jpg",
+    id: "tomhardy",
+    name: "회원님을 팔로우 합니다",
+  },
+  { image: "https://www.mens-hairstyle.com/wp-content/uploads/2020/01/1-thomas-shelby-haircut-2301202015301.jpg", id: "Thomas_Shelby", name: "Instagram 신규가입" },
+  {
+    image: "http://file3.instiz.net/data/cached_img/upload/2019/05/08/19/4862433a9fbdfa9bbb56c14e937eff17.jpg",
+    id: "tomholland2013",
+    name: "회원님을 팔로우 합니다",
+  },
+  { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl2ZBeM-vzNmOli_VOU8CjyaUiVAOvT4PeZg&usqp=CAU", id: "bts.bighitofficial", name: "인기" },
+  {
+    image:
+      "https://ww.namu.la/s/9b01072ce3e497c070c99c7574fb3e3e50e01f8c6f915fef15cc8b75ae3a2d3984481c84da0949d33d32b52679cf53dfd7981f1a5c6678ab39c12da807b492ef3c7085135c824d072ac1310d7d8ac6d26d44128b2e6740547f08bdd7ca3bd186",
+    id: "NF",
+    name: "Instagram 신규가입",
+  },
+];
+
+export const InformationO = [
+  { name: "소개" },
+  { name: "도움말" },
+  { name: "홍보" },
+  { name: "센터" },
+  { name: "API" },
+  { name: "채용 정보" },
+  { name: "개인정보처리방침" },
+  { name: "약관" },
+  { name: "위치" },
+  { name: "인기" },
+  { name: "계정" },
+  { name: "해시태그" },
+  { name: "언어" },
+];
