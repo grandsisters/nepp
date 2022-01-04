98c515f59f0c7f6ace2233f3f84fd416ed7f7cd9
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-04 14:29:55 +0900

    organisms

--- insta/src/components/organisms/Topnav.jsx
+++ insta/src/components/organisms/Topnav.jsx
@@ -44,7 +44,7 @@ const TopNav = () => {
       {showModal && <ModalTopNav onClose={handleModal} />}
       <Header>
         <HeaderWrapper>
-          <Link to="main">
+          <Link to="/main">
             <LogoImage src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" />
           </Link>
           <Nav>
@@ -53,7 +53,9 @@ const TopNav = () => {
             </SearchWrapper>
           </Nav>
           <NavMenu>
-            <ImgHome />
+            <Link to="/main">
+              <ImgHome />
+            </Link>
             <ImgMessage />
 
             <NewWrapper>{showModal ? <ImgSelectedNew /> : <ImgNew onClick={handleModal} />}</NewWrapper>
