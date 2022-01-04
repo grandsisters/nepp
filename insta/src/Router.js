3e0aa1fadf1d44893b4930a0547568600975e6c2
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-04 14:18:11 +0900

    src

--- insta/src/Router.js
+++ insta/src/Router.js
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Signup, Main, Profile } from "./components/pages";
+import { ProfileTagged, ProfileSaved, ProfileIndex, TopNav } from "./components/organisms";
+import { Login, Signup, Main, Profile, Landing, Setting } from "./components/pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<TopNav />}>
+          <Route path="main" element={<Main />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="profile" element={<Profile />}>
+            <Route path="index" element={<ProfileIndex />} />
+            <Route path="saved" element={<ProfileSaved />} />
+            <Route path="tagged" element={<ProfileTagged />} />
+          </Route>
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
