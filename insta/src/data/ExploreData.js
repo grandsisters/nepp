2977da2472e5690eff4cfd8f039fcf8ba1c1da75
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-06 21:14:00 +0900

    data

---
+++ insta/src/data/ExploreData.js
@@ -0,0 +1,14 @@
+export const ExploreData = [
+  "http://newsimg.hankookilbo.com/2019/10/30/201910301698735795_1.jpg",
+  "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/1029/IE002234416_STD.jpg",
+  "https://cdn.sisamagazine.co.kr/news/photo/201901/173591_169509_3035.jpg",
+  "https://image.newdaily.co.kr/site/data/img/2021/05/07/2021050700052_0.jpg",
+  "http://newsimg.hankookilbo.com/2019/10/30/201910301698735795_1.jpg",
+  "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/1029/IE002234416_STD.jpg",
+  "https://cdn.sisamagazine.co.kr/news/photo/201901/173591_169509_3035.jpg",
+  "https://image.newdaily.co.kr/site/data/img/2021/05/07/2021050700052_0.jpg",
+  "http://newsimg.hankookilbo.com/2019/10/30/201910301698735795_1.jpg",
+  "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/1029/IE002234416_STD.jpg",
+  "https://cdn.sisamagazine.co.kr/news/photo/201901/173591_169509_3035.jpg",
+  "https://image.newdaily.co.kr/site/data/img/2021/05/07/2021050700052_0.jpg",
+];
