8d3bdeda07047903034cd699c84eb36bb8fa258d
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-07 22:22:00 +0900

    insta CarouselImage

---
+++ insta/src/assets/images/InstaCarouselImg/index.js
@@ -0,0 +1,11 @@
+import ImgLandingCarousel1 from "./LandingCarouselImg1.png";
+import ImgLandingCarousel2 from "./LandingCarouselImg2.png";
+import ImgLandingCarousel3 from "./LandingCarouselImg3.png";
+import ImgLandingCarousel4 from "./LandingCarouselImg4.png";
+import ImgLandingCarousel5 from "./LandingCarouselImg5.png";
+import ImgLandingCarousel6 from "./LandingCarouselImg6.png";
+import ImgLandingCarousel7 from "./LandingCarouselImg7.png";
+
+const LandingImgList = [ImgLandingCarousel1, ImgLandingCarousel2, ImgLandingCarousel3, ImgLandingCarousel4, ImgLandingCarousel5, ImgLandingCarousel6, ImgLandingCarousel7];
+
+export default LandingImgList;
