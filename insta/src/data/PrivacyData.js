0ed9bcc211203b2c380fcd4a530bc54acc9f3044
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-08 18:15:58 +0900

    data

---
+++ insta/src/data/PrivacyData.js
@@ -0,0 +1,21 @@
+export const CheckBox = [
+  {
+    title: "계정 공개 범위",
+    checkname: "비공개 계정",
+    text: "계정이 비공개 상태인 경우 회원님이 승인한 사람만 Instagram에서 회원님의 사진과 동영상을 볼 수 있습니다. 기존 팔로워는 영향을 받지 않습니다.",
+  },
+  {
+    title: "활동 상태",
+    checkname: "활동 상태 표시",
+    text: "Instagram 앱에서 최근 활동한 시간 정보가 회원님이 팔로우하는 계정 및 메시지를 보낸 모든 사람에게 표시됩니다. 이 설정을 해제하면 다른 계정의 활동 상태를 볼 수 없습니다.",
+  },
+  { title: "스토리 공유", checkname: "공유 허용", text: "사람들이 회원님의 스토리를 메시지로 공유할 수 있습니다." },
+];
+
+export const LinkBox = [
+  { title: "댓글", linkname: "댓글 설정 수정" },
+  { title: "계정 데이터", linkname: "계정 데이터 보기" },
+  { title: "2단계 인증", linkname: "2단계 인증 설정 수정" },
+  { title: "데이터 다운로드", linkname: "다운로드 요청" },
+  { title: "개인정보 및 보안 도움말", linkname: "지원" },
+];
---
+++ insta/src/data/PushData.js
@@ -0,0 +1,105 @@
+export const PushData = [
+  {
+    title: "좋아요",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "내가 팔로우하는 사람", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "johnappleseed님이 회원님의 사진을 좋아합니다",
+  },
+
+  {
+    title: "댓글",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "내가 팔로우하는 사람", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: 'johnappleseed님이 댓글을 남겼습니다: "멋지네요!"',
+  },
+
+  {
+    title: "댓글 좋아요",
+    labels: [
+      { label: "해제", checked: "1" },
+      { label: "내가 팔로우하는 사람", checked: "" },
+    ],
+    text: 'johnappleseed님이 회원님의 댓글을 좋아합니다: "멋지네요!"',
+  },
+  {
+    title: "회원님이 나온 사진의 좋아요 및 댓글",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "내가 팔로우하는 사람", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "johnappleseed님이 회원님이 태그된 게시물에 댓글을 남겼습니다.",
+  },
+  {
+    title: "수락한 팔로우 요청",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "John Appleseed(@johnappleseed)님이 팔로우 요청을 수락했습니다.",
+  },
+  {
+    title: "Instagram Direct 요청",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "johnappleseed님이 메시지를 보내고 싶어합니다.",
+  },
+  {
+    title: "Instagram Direct",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "johnappleseed님이 메시지를 보냈습니다.",
+  },
+  {
+    title: "알림",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "'알림을 읽지 않으셨습니다' 등을 안내하는 알림입니다.",
+  },
+
+  {
+    title: "첫 게시물과 스토리",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "내가 팔로우하는 사람", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "johnappleseed님의 첫 Instagram 스토리 게시 등을 안내하는 알림입니다.",
+  },
+  {
+    title: "동영상 조회수",
+    labels: [
+      { label: "해제", checked: "" },
+      { label: "모든 사람", checked: "1" },
+    ],
+    text: "회원님의 동영상 조회수가 10만회를 넘었습니다.",
+  },
+  {
+    title: "지원 요청",
+    labels: [
+      { label: "해제", checked: "1" },
+      { label: "설정", checked: "" },
+    ],
+    text: "7월 10에 요청하신 지원에 대한 내용이 업데이트되었습니다.",
+  },
+  {
+    title: "라이브 방송",
+    labels: [
+      { label: "해제", checked: "1" },
+      { label: "설정", checked: "" },
+    ],
+    text: "johnappleseed님이 라이브 방송을 시작했습니다. 지금 바로 시청해보세요!",
+  },
+];
