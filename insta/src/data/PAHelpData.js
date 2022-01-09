57cb7e1b70a904e7ececc4d65e89c615efa8634e
grandsisters <59759141+grandsisters@users.noreply.github.com>
2022-01-09 22:25:18 +0900

    data

---
+++ insta/src/data/PAHelpData.js
@@ -0,0 +1,26 @@
+export const PAHelpData = [
+  { title: "계정 관리", content: [{ subtitle: "연령 제한" }, { subtitle: "계정 공개 범위" }, { subtitle: "위치 공유" }, { subtitle: "2단계 인증" }] },
+  {
+    title: "내 Instagram 환경 설정",
+    content: [
+      { subtitle: "계정 차단" },
+      { subtitle: "팔로워 삭제" },
+      { subtitle: "내가 나온 사진 관리" },
+      { subtitle: "댓글 필터링" },
+      { subtitle: "댓글 기능 해제" },
+      { subtitle: "댓글 삭제" },
+      { subtitle: "내 스토리를 볼 수 있는 사람 선택" },
+      { subtitle: "내 스토리에 답장을 보내도록 허용할 사람 선택" },
+    ],
+  },
+  {
+    title: "마음에 들지 않는 콘텐츠 신고",
+    content: [
+      { subtitle: "댓글 신고" },
+      { subtitle: "계정 또는 게시물 신고" },
+      { subtitle: "당사자가 성적 수치심을 느낄 만한 이미지 신고" },
+      { subtitle: "탐색 탭에서 콘텐츠 삭제" },
+      { subtitle: "민감한 내용이 포함된 콘텐츠 화면" },
+    ],
+  },
+];
---
+++ insta/src/data/SecurityData.js
@@ -0,0 +1,4 @@
+export const SecurityData = [
+  { text: "grandsisters님, 더욱 간편하게 Instagram에 다시 로그인해보세요", time: "2022년 1월 7일 오후 1:04", reciver: "받는 사람: grandsisters@gmail.com · security@mail.instagram.com에서 전송됨" },
+  { text: "Confirm your email address for Instagram", time: "2022년 1월 6일 오후 7:42", reciver: "받는 사람: grandsisters@gmail.com · no-reply@mail.instagram.com에서 전송됨" },
+];
